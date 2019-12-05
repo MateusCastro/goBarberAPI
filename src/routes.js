f1450d115e95461c6492e4a5ed01cb6e0256fff4
Mateus Castro <mateus.castro.domingos@gmail.com>
2019-12-05 13:40:48 -0300

    Criado loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ messege: "Hello world" });
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "Diego Fernandes",
+    email: "diego@gmail.com",
+    password_hash: "2903784293"
+  });
+
+  return res.json(user);
 });
 
 export default routes;
