3bb6fb82c2ab9eba983481d7a25a5173af1ab75d
Mateus Castro <mateus.castro.domingos@gmail.com>
2020-01-01 23:48:06 -0300

    Listar horário disponíveis do appointment finalizado

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,8 +5,8 @@ import Appointment from "../models/Appointment";
 import User from "../models/User";
 import File from "../models/File";
 import Notification from "../schemas/Notification";
-/*import CancellationMail from "../jobs/CancellationMail";
-import Queue from "../../lib/Queue";*/
+import CancellationMail from "../jobs/CancellationMail";
+import Queue from "../../lib/Queue";
 
 class AppointmentController {
   async index(req, res) {
@@ -164,7 +164,7 @@ class AppointmentController {
 
     await appointment.save();
 
-    // await Queue.add(CancellationMail.key, { appointment });
+    await Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
